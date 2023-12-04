93d722fd32eda1a724d49e0766df7a4be89c18f3
Yongveloper <aaa9015@naver.com>
2023-12-05 01:20:59 +0900

    feat: 질문 옵션 추가 기능

--- src/components/QuestionForm.tsx
+++ src/components/QuestionForm.tsx
@@ -1,6 +1,10 @@
 import React from 'react';
 import styled from 'styled-components';
-import { setInputType, setTitle } from '@store/slices/questionSlice';
+import {
+  addInputItem,
+  setInputType,
+  setTitle,
+} from '@store/slices/questionSlice';
 import Box from '@mui/system/Box';
 import FormContainer from './FormContainer';
 import RadioButtonUncheckedIcon from '@mui/icons-material/RadioButtonUnchecked';
@@ -22,7 +26,7 @@ const TopContainer = styled.div`
 const ContentsContainer = styled.div`
   .input__default {
     padding: 10px;
-    color: rgba(0, 0, 0, 0.38);
+    color: ${({ theme }) => theme.colors.lightGrey};
     border-bottom: 1px solid ${({ theme }) => theme.colors.grey};
   }
 
@@ -41,22 +45,44 @@ const ContentsContainer = styled.div`
   }
 `;
 
-// const STextField = styled(TextField)`
-//   width: 100%;
-//   max-width: 446px;
-//   input {
-//     padding: 16px;
-//   }
-// `;
+const AddItemBtnWrapper = styled.div`
+  display: flex;
+  align-items: center;
+  height: 48px;
+
+  .AddItemBtn {
+    margin-left: 10px;
+    font-size: 14px;
+    color: ${({ theme }) => theme.colors.lightGrey};
+    &:hover {
+      cursor: pointer;
+      border-bottom: 1px solid ${({ theme }) => theme.colors.grey};
+    }
+  }
+`;
 
 interface IQuestionFormProps {
   id: string;
 }
 
 function QuestionForm({ id }: IQuestionFormProps) {
-  const question = useAppSelector((state) =>
-    state.question.find((question) => question.id === id)
+  const isFocused = useAppSelector(
+    (state) => state.question.find((question) => question.id === id)?.isFocused
   );
+
+  const inputType = useAppSelector(
+    (state) =>
+      state.question.find((question) => question.id === id)?.inputType as string
+  );
+
+  const title = useAppSelector(
+    (state) => state.question.find((question) => question.id === id)?.title
+  );
+
+  const contents = useAppSelector(
+    (state) => state.question.find((question) => question.id === id)?.contents
+  );
+
   const dispatch = useAppDispatch();
   const handleTitle = (e: React.ChangeEvent<HTMLInputElement>) => {
     dispatch(setTitle({ id, contents: e.target.value }));
@@ -66,25 +92,37 @@ function QuestionForm({ id }: IQuestionFormProps) {
     dispatch(setInputType({ id, contents: e.target.value }));
   };
 
+  const handleAddInputItem = () => {
+    if (Array.isArray(contents)) {
+      dispatch(
+        addInputItem({
+          id,
+          contentId: String(Date.now()),
+          text: `옵션 ${contents.length + 1}`,
+        })
+      );
+    }
+  };
+
   return (
     <FormContainer id={id}>
       <>
         <TopContainer>
-          {!question?.isFocused && <Box>{question?.title}</Box>}
-          {question?.isFocused && (
+          {!isFocused && <Box>{title}</Box>}
+          {isFocused && (
             <STextField
               sx={{ maxWidth: '446px' }}
               inputProps={{ style: { padding: 16 } }}
               id="filled-search"
               type="search"
               variant="filled"
-              value={question?.title}
+              value={title}
               onChange={handleTitle}
             />
           )}
-          {question?.isFocused && (
+          {isFocused && (
             <FormControl sx={{ minWidth: 208 }}>
-              <Select value={question?.inputType} onChange={handleInputType}>
+              <Select value={inputType} onChange={handleInputType}>
                 <MenuItem value="shortAnswer">단답형</MenuItem>
                 <MenuItem value="longAnswer">장문형</MenuItem>
                 <MenuItem value="radio">객관식 질문</MenuItem>
@@ -95,24 +133,40 @@ function QuestionForm({ id }: IQuestionFormProps) {
           )}
         </TopContainer>
         <ContentsContainer>
-          {question?.inputType === 'shortAnswer' && (
+          {inputType === 'shortAnswer' && (
             <div className="input__default short-textfield">단문형 텍스트</div>
           )}
-          {question?.inputType === 'longAnswer' && (
+          {inputType === 'longAnswer' && (
             <div className="input__default long-textfield">장문형 텍스트</div>
           )}
-          {Array.isArray(question?.contents) &&
-            question?.contents.map((content, index) => (
+          {Array.isArray(contents) &&
+            contents.map((content, index) => (
               <div className="input-items" key={index}>
-                {question?.inputType === 'radio' && (
-                  <RadioButtonUncheckedIcon />
-                )}
-                {question?.inputType === 'checkbox' && <CropSquareIcon />}
-                {question?.inputType === 'dropdown' && <span>{index + 1}</span>}
+                {inputType === 'radio' && <RadioButtonUncheckedIcon />}
+                {inputType === 'checkbox' && <CropSquareIcon />}
+                {inputType === 'dropdown' && <span>{index + 1}</span>}
 
                 <span>{content.text}</span>
               </div>
             ))}
+          {isFocused && (
+            <AddItemBtnWrapper>
+              {
+                {
+                  radio: <RadioButtonUncheckedIcon />,
+                  checkbox: <CropSquareIcon />,
+                  dropdown: (
+                    <span>
+                      {Array.isArray(contents) && contents.length + 1}
+                    </span>
+                  ),
+                }[inputType]
+              }
+              <span className="AddItemBtn" onClick={handleAddInputItem}>
+                옵션 추가
+              </span>
+            </AddItemBtnWrapper>
+          )}
         </ContentsContainer>
       </>
     </FormContainer>

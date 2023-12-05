59dd72c255f9a3463295816feb95b109c4c358c4
Yongveloper <aaa9015@naver.com>
2023-12-05 16:45:40 +0900

    feat: TextField hover 효과

--- src/components/common/STextField.styles.ts
+++ src/components/common/STextField.styles.ts
@@ -4,22 +4,18 @@ import TextField from '@mui/material/TextField';
 export const STextField = styled(TextField)`
   width: 100%;
 
-  & .MuiInput-underline:before {
-    border-bottom: 1px solid ${({ theme }) => theme.colors.grey};
-  }
-  & .MuiInput-underline:after {
-    border-bottom-color: ${({ theme }) => theme.colors.purple};
-  }
-  & .MuiInput-underline:hover:not(.Mui-disabled):before {
-    border-bottom: 1px solid ${({ theme }) => theme.colors.grey};
+  & .MuiInput-underline:before,
+  & .MuiFilledInput-underline:before {
+    border-bottom: none;
+    border-bottom-color: ${({ theme }) => theme.colors.grey};
   }
 
-  & .MuiFilledInput-underline:before {
-    border-bottom: 1px solid ${({ theme }) => theme.colors.grey};
-  }
+  & .MuiInput-underline:after,
   & .MuiFilledInput-underline:after {
-    border-bottom-color: ${({ theme }) => theme.colors.purple};
+    border-bottom: 2px solid ${({ theme }) => theme.colors.purple};
   }
+
+  & .MuiInput-underline:hover:not(.Mui-disabled):before,
   & .MuiFilledInput-underline:hover:not(.Mui-disabled):before {
     border-bottom: 1px solid ${({ theme }) => theme.colors.grey};
   }

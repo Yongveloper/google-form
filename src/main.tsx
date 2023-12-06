805833f70bd37ab1cb4ce95b924471d9356e102a
Yongveloper <aaa9015@naver.com>
2023-12-07 02:37:16 +0900

    feat: MUI 기본 색상 변경

--- src/main.tsx
+++ src/main.tsx
@@ -2,21 +2,33 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import { Provider } from 'react-redux';
 import { PersistGate } from 'redux-persist/integration/react';
-import { ThemeProvider } from 'styled-components';
+import { ThemeProvider as StyledProvider } from 'styled-components';
+import {
+  createTheme,
+  ThemeProvider as MuiProvider,
+} from '@mui/material/styles';
 import { GlobalStyles } from '@styles/GlobalStyles.ts';
 import { theme } from '@styles/theme.ts';
 import { persistor, store } from '@store/store.ts';
 import App from './App.tsx';
 
+const muiTheme = createTheme({
+  palette: {
+    primary: {
+      main: theme.colors.purple,
+    },
+  },
+});
+
 ReactDOM.createRoot(document.getElementById('root')!).render(
-  // <React.StrictMode>
   <Provider store={store}>
     <PersistGate loading={null} persistor={persistor}>
-      <ThemeProvider theme={theme}>
-        <GlobalStyles />
-        <App />
-      </ThemeProvider>
+      <MuiProvider theme={muiTheme}>
+        <StyledProvider theme={theme}>
+          <GlobalStyles />
+          <App />
+        </StyledProvider>
+      </MuiProvider>
     </PersistGate>
   </Provider>
-  // </React.StrictMode>
 );
